c0c337680a34c02ec5a127830374721675548cf9
yixiaoming <yixiaoming.me@qq.com>
2019-08-19 00:01:05 +0800

    拆分组件化结构，遗留问题 okhttp lib不能引用

---
+++ modules/kaiyan/src/main/java/org/miles/kaiyan/ModuleActivity.java
@@ -0,0 +1,15 @@
+package org.miles.kaiyan;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_activity);
+    }
+}
